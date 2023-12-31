b869c8c56c382ee14c57441a47d3f2384e9fb797
Arabaz Bagwan <arabazbagwan@gmail.com>
2023-12-31 16:02:19 +0530

    my commit -1
    1st commit from ab

--- backend/main.go
+++ backend/main.go
@@ -8,40 +8,64 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/avast/retry-go"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"time"
 )
 
-func main() {
+const maxRetries = 5
+const retryDelay = 1 * time.Second
 
+func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: os.Getenv("ALLOWED_ORIGINS"),
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
-	db := initializeDatabaseConnection()
+
+	var db *gorm.DB
+	err := retry.Do(
+		func() error {
+			db = initializeDatabaseConnection()
+			err := db.Raw("SELECT 1").Scan(&struct{}{}).Error
+			if err != nil {
+				log.Println("Error connecting to the database:", err)
+				return err
+			}
+			return nil
+		},
+		retry.Attempts(maxRetries),
+		retry.Delay(retryDelay),
+	)
+
+	if err != nil {
+		log.Fatal("Failed to connect to the database after multiple attempts.")
+	}
+
 	repository.RunMigrations(db)
 	employeeRepository := repository.NewEmployeeRepository(db)
 	employeeService := service.NewEmployeeService(employeeRepository)
 	employeeController := controller.NewEmployeeController(employeeService)
 	routes.RegisterRoute(app, employeeController)
-	err := app.Listen(":8080")
+
+	err = app.Listen(":8080")
 	if err != nil {
 		log.Fatalln(fmt.Sprintf("error starting the server %s", err.Error()))
 	}
 }
+
 func initializeDatabaseConnection() *gorm.DB {
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  createDsn(),
 		PreferSimpleProtocol: true,
 	}), &gorm.Config{})
 	if err != nil {
-		log.Fatalln(fmt.Sprintf("error connecting with database %s", err.Error()))
+		log.Println(fmt.Sprintf("error connecting with database %s", err.Error()))
 	}
-
 	return db
 }
 
